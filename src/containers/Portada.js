e2dfc0bbebda8faa0a67e77574ba59925ccccec9
Jeisson Villamarin <jeissonvillamarin1@gmail.com>
2021-11-30 22:20:18 -0500

    cover page

--- src/containers/Portada.js
+++ src/containers/Portada.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Container} from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 export const Portada = () => {
   return (
@@ -14,12 +15,14 @@ export const Portada = () => {
 
         }}
       >
-        <img
-          src="https://res.cloudinary.com/dtp6uf9vc/image/upload/v1638301437/Agendas%20Digitales/logo-portada_decgfg.png"
-          alt="Agendas Digitales"
-          width="150px"
-          height="200px"
-        />
+        <Link to="/bienvenida">
+          <img
+            src="https://res.cloudinary.com/dtp6uf9vc/image/upload/v1638301437/Agendas%20Digitales/logo-portada_decgfg.png"
+            alt="Agendas Digitales"
+            width="150px"
+            height="200px"
+          />
+        </Link>
       </Container>
     </>
   );

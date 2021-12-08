dcc98a4801b21038032bd4415eb321307ba09e38
Jeisson Villamarin <jeissonvillamarin1@gmail.com>
2021-12-08 18:03:21 -0500

    merging with main

--- src/routers/RoutesApp.js
+++ src/routers/RoutesApp.js
@@ -3,13 +3,8 @@ import { Routes, Route } from 'react-router-dom';
 import { Home } from '../containers/Home'
 import { Calendario } from '../containers/Calendario';
 import { Perfil } from '../containers/Perfil';
-<<<<<<< HEAD
-import {TodoApp} from '../components/todo/Todo';
-import { Metas } from '../containers/Metas';
-=======
 import { Tareas } from '../containers/Tareas';
 import { CheckList } from '../containers/CheckList';
->>>>>>> jeisson
 
 
 const RoutesApp = () => {
@@ -19,13 +14,8 @@ const RoutesApp = () => {
           <Route path="/home" element={<Home/>} />
           <Route path="/calendario" element={<Calendario/>} />
           <Route path="/perfil" element={<Perfil/>} />
-<<<<<<< HEAD
-          <Route path="/tareas" element={<TodoApp/>} />
-          <Route path="/metas" element={<Metas/>} />
-=======
           <Route path="/tareas" element={<Tareas/>} />
           <Route path="/checklist" element={<CheckList/>} />
->>>>>>> jeisson
         </Routes>
         </>
   );
